44fa9c7509011efbcac4ecca2adb2470f40cc20c
Jasper Lin <jasper.lin@berkeley.edu>
2023-06-15 23:20:27 -0400

    Add message limit

--- cheerleader-api/chat/chat_consumer.py
+++ cheerleader-api/chat/chat_consumer.py
@@ -1,6 +1,7 @@
 from enum import StrEnum
 
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from django.conf import settings
 
 from chat.bots.utils import generate_bot
 
@@ -13,6 +14,7 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.bot = generate_bot()
+        self.user_message_count = 0
 
     async def connect(self):
         await self.accept()
@@ -21,10 +23,15 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         if content["type"] != self.MessageType.CHAT_MESSAGE:
             return
 
-        message = content["data"]
-        bot_message = self.bot.predict(
-            user_message=message
-        )
+        self.user_message_count += 1
+        if self.user_message_count > settings.MAX_USER_MESSAGE_COUNT:
+            bot_message = f"You've reached your message limit! You can contact Jasper at {settings.EMAIL_ADDRESS} or " \
+                          f"{settings.PHONE_NUMBER}."
+        else:
+            message = content["data"]
+            bot_message = self.bot.predict(
+                user_message=message
+            )
 
         await self.send_json({
             "type": self.MessageType.BOT_MESSAGE,
