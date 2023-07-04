caca2afc1d08c75ed23b96a703cfe997dc1d8fe8
Jasper Lin <jasper.lin@berkeley.edu>
2023-07-03 20:33:46 -0400

    csrf

--- cheerleader/views.py
+++ cheerleader/views.py
@@ -1,6 +1,5 @@
-from django.http import JsonResponse, HttpRequest, HttpResponse
-from django.middleware.csrf import get_token
+from django.http import HttpRequest, HttpResponse
 
 
-def csrf(request: HttpRequest) -> HttpResponse:
-    return JsonResponse({'csrfToken': get_token(request)})
+def csrf(_: HttpRequest) -> HttpResponse:
+    return HttpResponse('CSRF token set.')
