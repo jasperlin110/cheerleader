87287efb48f040cec4ce3fdd42035118ce7fe7a7
Jasper Lin <jasper.lin@berkeley.edu>
2023-07-04 20:13:41 -0400

    save bot memory in session

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 from datetime import datetime, timezone
 from json import JSONDecodeError
 
@@ -7,6 +8,7 @@ from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseBad
 from django.views.decorators.http import require_POST
 from jsonschema.exceptions import ValidationError
 from jsonschema.validators import validate
+from langchain.schema import messages_to_dict, messages_from_dict
 
 from chat.bots import utils
 
@@ -43,16 +45,21 @@ def bot_response(request: HttpRequest) -> HttpResponse:
 
     user_message = request_body.get("message")
 
-    request.session.setdefault("message_count", 0)
-    request.session["message_count"] += 1
+    message_count = request.session.get("message_count", 0)
+    request.session["message_count"] = message_count + 1
     if request.session["message_count"] > settings.MAX_USER_MESSAGE_COUNT:
         bot_message = f"You've reached your message limit! You can contact Jasper at {settings.EMAIL_ADDRESS} or " \
                       f"{settings.PHONE_NUMBER}."
     else:
-        bot = utils.generate_bot()
+        messages = request.session.get("chat_messages")
+        if messages:
+            messages = messages_from_dict(messages)
+
+        bot = utils.generate_bot(messages)
         bot_message = bot.predict(
             user_message=user_message
         )
+        request.session["chat_messages"] = messages_to_dict(bot.memory.chat_memory.messages)
 
     return JsonResponse({
         "role": "bot",
